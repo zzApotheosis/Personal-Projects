d9e32848205936068b1442fa4d72a4bae55b65eb
Steven Jennings <zzApotheosis@gmail.com>
2019-08-09 11:44:29 -0500

    .

--- Golang Practice/src/zzCrypt/main.go
+++ Golang Practice/src/zzCrypt/main.go
@@ -1,5 +1,5 @@
 /*
- * TODO: Cleared everything out of this project for a rewrite. This will be a Linux-only project.
+ * TODO: Cleared everything out of this project for a rewrite. This will be a Linux-only project utilizing tar and gpg to encrypt/decrypt data. This ought to become a more useful project than what it was.
  */
 
 package main
