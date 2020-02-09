93587644c42de66e95ce2be1e67736f14270010d
zzApotheosis <zzApotheosis@gmail.com>
2020-02-09 15:03:42 -0600

    Update

--- Rust Practice/testbench/src/main.rs
+++ Rust Practice/testbench/src/main.rs
@@ -8,32 +8,48 @@ fn main() {
     let smtp_username = "zzApotheosis@gmail.com";
     let smtp_password = "gruxigzwkpbdxljj";
     let smtp_port: u16 = 587;
+    
+    let email = SendableEmail::new(
+        Envelope::new(
+            Some(EmailAddress::new("user@localhost".to_string()).unwrap()),
+            vec![EmailAddress::new("root@localhost".to_string()).unwrap()],
+        ).unwrap(),
+        "id".to_string(),
+        "Hello world".to_string().into_bytes(),
+    );
 
-    let email = EmailBuilder::new()
-        .to(to_address)
-        .from(smtp_username)
-        .subject("Hi, Hello world")
-        .body("Hello world.")
-        //.attachment_from_file(Path::new("Cargo.toml"), None, &TEXT_PLAIN)
-        .build()
-        .unwrap();
+    let mut mailer =
+        SmtpClient::new_unencrypted_localhost().unwrap().transport();
+
+    let result = mailer.send(email);
+
+    //assert!(result.is_ok());
+
+//    let email = EmailBuilder::new()
+//        .to(to_address)
+//        .from(smtp_username)
+//        .subject("Hi, Hello world")
+//        .body("Hello world.")
+//        //.attachment_from_file(Path::new("Cargo.toml"), None, &TEXT_PLAIN)
+//        .build()
+//        .unwrap();
 
     // Open a local connection on port 25
-    let mut mailer = SmtpTransportBuilder::new((sntp_server, smtp_port)).unwrap
-        .hello_name("localhost")
-        .credentials(smtp_username, smtp_password)
-        .security_level(SecurityLevel::AlwaysEncrypt)
-        .smtp_utf8(true)
-        .build();
+    //let mut mailer = SmtpTransportBuilder::new((sntp_server, smtp_port)).unwrap
+    //    .hello_name("localhost")
+    //    .credentials(smtp_username, smtp_password)
+    //    .security_level(SecurityLevel::AlwaysEncrypt)
+    //    .smtp_utf8(true)
+    //    .build();
 
     // Send the email
-    let result = mailer.send(email.clone());
+    //let result = mailer.send(email.clone());
 
-    if result.is_ok() {
-        println!("Email sent");
-    } else {
-        println!("Could not send email: {:?}", result);
-    }
+    //if result.is_ok() {
+    //    println!("Email sent");
+    //} else {
+    //    println!("Could not send email: {:?}", result);
+    //}
 
     //assert!(result.is_ok());
 }
---
+++ Rust Practice/testbench/target/debug/testbench.d
@@ -0,0 +1,1 @@
+/home/zzapotheosis/Development/GitHub/Personal-Projects/Rust\ Practice/testbench/target/debug/testbench: /home/zzapotheosis/Development/GitHub/Personal-Projects/Rust\ Practice/testbench/src/main.rs
