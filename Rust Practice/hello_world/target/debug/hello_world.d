1e51b572548f6801cc6fed02817f6fc00cfcde9f
zzApotheosis <zzApotheosis@gmail.com>
2020-02-09 12:02:24 -0600

    Create Rust Practice

---
+++ Rust Practice/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/zzapotheosis/Development/GitHub/Personal-Projects/Rust\ Practice/hello_world/target/debug/hello_world: /home/zzapotheosis/Development/GitHub/Personal-Projects/Rust\ Practice/hello_world/src/main.rs
