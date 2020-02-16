5a09a618125d81cf474dd10579f5cb9511b0c492
zzApotheosis <zzApotheosis@gmail.com>
2020-02-16 12:15:49 -0600

    Update package list

---
+++ Rust Practice/hello_world/target/x86_64-pc-windows-gnu/debug/deps/hello_world-d71c4077d84de431.d
@@ -0,0 +1,5 @@
+/home/zzapotheosis/Development/GitHub/Personal-Projects/Rust Practice/hello_world/target/x86_64-pc-windows-gnu/debug/deps/hello_world-d71c4077d84de431.exe: src/main.rs
+
+/home/zzapotheosis/Development/GitHub/Personal-Projects/Rust Practice/hello_world/target/x86_64-pc-windows-gnu/debug/deps/hello_world-d71c4077d84de431.d: src/main.rs
+
+src/main.rs:
---
+++ Rust Practice/hello_world/target/x86_64-pc-windows-msvc/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+/home/zzapotheosis/Development/GitHub/Personal-Projects/Rust Practice/hello_world/target/x86_64-pc-windows-msvc/debug/deps/hello_world.exe: src/main.rs
+
+/home/zzapotheosis/Development/GitHub/Personal-Projects/Rust Practice/hello_world/target/x86_64-pc-windows-msvc/debug/deps/hello_world.d: src/main.rs
+
+src/main.rs:
