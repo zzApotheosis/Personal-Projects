6cf4dc642d935bcb65556909de471270381e9217
zzApotheosis <zzApotheosis@gmail.com>
2021-03-31 19:39:18 -0500

    Add Perl practice
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

---
+++ Programming Practice/Perl/template.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+# Imports
+use strict;
+use warnings;
+use FindBin;
+use Cwd;
+
+# Global variables
+my $script_name = "$FindBin::RealScript";
+my $script_path = "$FindBin::RealBin/$FindBin::RealScript";
+my $script_dir = "$FindBin::RealBin";
+my $original_cwd = getcwd();
+
+# Main
+sub main {
+    # Define subroutine variables
+    my $exit_code = 0;
+    
+    # Do code
+    print(STDOUT "Script name: $script_name\n");
+    print(STDOUT "Script path: $script_path\n");
+    print(STDOUT "Script dir: $script_dir\n");
+    print(STDOUT "Original cwd: $original_cwd\n");
+    
+    # Done
+    return $exit_code;
+}
+
+# Execute
+exit(main());
+
