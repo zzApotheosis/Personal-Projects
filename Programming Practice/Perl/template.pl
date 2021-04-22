233626d7a3c76b74550f67e1621803e1bb6235ac
Steven <zzApotheosis@gmail.com>
2021-04-22 10:35:17 -0500

    Update template.pl

--- Programming Practice/Perl/template.pl
+++ Programming Practice/Perl/template.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+# Main Class
+package Main;
+
 # Imports
 use strict;
 use warnings;
@@ -28,5 +31,7 @@ sub main {
 }
 
 # Execute
-exit(main());
+exit(Main::main());
 
+# End Main Class
+1;
