5d53a58755d16d7e04c50da44b1d92388af39f49
zzApotheosis <zzApotheosis@gmail.com>
2021-05-02 09:27:55 -0500

    Update example Assembly code
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- Programming Practice/Assembly/test/hello.s
+++ Programming Practice/Assembly/test/hello.s
@@ -1,17 +1,26 @@
+# ----------------------------------------------------------------------------------------
+# Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+# To assemble and run:
+#
+#     gcc -c hello.s && ld hello.o && ./a.out
+#
+# ----------------------------------------------------------------------------------------
+
     .global _start
     
     .text
 _start:
     # write(1, message, 13)
-    mov     $1, %rax
-    mov     $1, %rdi
-    mov     $message, %rsi
-    mov     $13, %rdx
-    syscall
-
-    mov     $60, %rax
-    xor     %rdi, %rdi
-    syscall
+    mov     $1, %rax            # system call 1 is write
+    mov     $1, %rdi            # file handle 1 is stdout
+    mov     $message, %rsi      # address of string to output
+    mov     $13, %rdx           # number of bytes
+    syscall                     # invoke operating system to do the write
+    
+    # exit(0)
+    mov     $60, %rax           # system call 60 is exit
+    xor     %rdi, %rdi          # we want return code 0
+    syscall                     # invoke operating system to exit
 message:
     .ascii  "Hello, world\n"
 
