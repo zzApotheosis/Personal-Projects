2fe85cad31e3cba0c629ebe4d27dfcc1bb0e5c76
zzApotheosis <zzApotheosis@gmail.com>
2021-05-04 06:42:12 -0500

    Add signal handler
    This needs to be cleaned up later
    
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- journaldlistener/src/main.c
+++ journaldlistener/src/main.c
@@ -1,8 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 #include "main.h"
 
+/* Forward declarations */
+void sigint_handler(int);
+
+/* Main */
 int main(int argc, char** argv) {
+    signal(SIGINT, sigint_handler);
     unixsocket_example();
     return EXIT_SUCCESS;
 }
+
+/* SIGINT handler function */
+void sigint_handler(int sig) {
+    fprintf(stdout, "Received SIGINT. Exiting\n");
+    unlink("socket");
+    exit(EXIT_SUCCESS);
+}
+
