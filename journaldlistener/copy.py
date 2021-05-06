5dcf2242059a3bca83739c2d80d452458305432d
zzApotheosis <zzApotheosis@gmail.com>
2021-05-05 22:40:20 -0500

    Finished a decent version of copy.py
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- journaldlistener/copy.py
+++ journaldlistener/copy.py
@@ -3,6 +3,7 @@
 # Imports
 import sys, os
 import argparse
+import shutil
 
 # Global variables
 exec_name = os.path.basename(__file__)
@@ -12,6 +13,10 @@ original_cwd = os.getcwd()
 
 # Main Class
 class Main:
+    # Class fields
+    argv = None
+    parser = None
+
     @staticmethod
     def main():
         # Define method variables
@@ -20,17 +25,39 @@ class Main:
         # Parse arguments
         Main.parse_args()
 
+        # Check for source file
+        if Main.argv.source is None:
+            Main.parser.print_help()
+            return 1
+
+        # Check for destination file
+        if Main.argv.destination is None:
+            Main.parser.print_help()
+            return 1
+
+        # Check if source file exists
+        if not os.path.exists(Main.argv.source):
+            print("No such file or directory", file=sys.stderr)
+            return 1
+
+        # Copy source file to destination
+        shutil.copyfile(Main.argv.source, Main.argv.destination)
+        
+
         # End main method
         return exit_code
 
     @staticmethod
     def parse_args():
         # Define method variables
-        parser = argparse.ArgumentParser(description="A simple tool to copy files using Python.")
+        Main.parser = argparse.ArgumentParser(description="A simple tool to copy files using Python.")
 
         # Define arguments to handle
-        parser.add_argument('-s', '--source', help="Specify the source file")
-        parser.add_argument('-d', '--destination', help="Specify the destination")
+        Main.parser.add_argument('-s', '--source', help="Specify the source file")
+        Main.parser.add_argument('-d', '--destination', help="Specify the destination")
+
+        # Parse args
+        Main.argv = Main.parser.parse_args()
 
 # Execute
 if __name__ == '__main__':
