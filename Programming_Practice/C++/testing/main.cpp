c573c7b093ca56ecae26ea5957930c21e2e29aa6
zzApotheosis <zzApotheosis@gmail.com>
2021-05-30 07:44:21 -0500

    Add C++ Practice
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

---
+++ Programming_Practice/C++/testing/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, world!\n";
+    fprintf(stdout, "Test\n");
+    return EXIT_SUCCESS;
+}
+
