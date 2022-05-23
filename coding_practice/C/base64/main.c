5e3183395d45f4f054a123db3b2ee21886c7af3c
zzApotheosis <zzApotheosis@gmail.com>
2022-05-23 17:54:37 -0500

    Add base64 encoding example
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

---
+++ coding_practice/C/base64/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <glib-2.0/glib.h>
+#include <glib-2.0/glib/gprintf.h>
+
+/*
+ * NOTE: This implementation uses the GLib library from the
+ * GNOME Foundation's GNOME Project. Needless to say, this
+ * implementation is generally only available on *nix systems.
+ *
+ * https://docs.gtk.org/glib/index.html
+ */
+
+int main(int argc, char ** argv) {
+    guint8 data[] = "Here is a sample string which we will encode using the "
+                    "base64 encoding scheme. If there's anybody even reading "
+                    "this, go play NieR:Automata, one of my favorite games.";
+    g_printf("Original text:\n%s\n", data);
+    guint8 * encoded_data = g_base64_encode(data, sizeof(data));
+    g_printf("\nEncoded text:\n%s\n", encoded_data);
+    gsize decoded_data_len = 0;
+    guint8 * decoded_data = g_base64_decode(encoded_data, &decoded_data_len);
+    g_printf("\nDecoded text:\n%s\n", decoded_data);
+    g_free(encoded_data);
+    g_free(decoded_data);
+    return(EXIT_SUCCESS);
+}
