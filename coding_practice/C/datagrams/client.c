882abbfbe77d561d8a02b7497427e8ab8f1f5426
zzApotheosis <zzApotheosis@gmail.com>
2022-06-02 21:34:10 -0500

    Add datagram (UDP) sample code
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

---
+++ coding_practice/C/datagrams/client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT 41454
+#define MAXLINE 1024
+
+int main(int argc, char ** argv) {
+    int sockfd;
+    char buffer[MAXLINE];
+    char * hello = "Hello from client";
+    struct sockaddr_in servaddr;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        fprintf(stdout, "line %d: Error caught\n", __LINE__);
+        abort();
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int n, len;
+
+    sendto(sockfd, (const char *) hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    n = recvfrom(sockfd, buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+    fprintf(stdout, "Client received message from server: %s\n", buffer);
+
+    close(sockfd);
+
+    return(EXIT_SUCCESS);
+}
---
+++ coding_practice/C/datagrams/server.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define PORT 41454 // Port randomly selected between 1-65536
+#define MAXLINE 1024
+
+const char hello[] = "Hello from server!";
+
+void handle_client(int sockfd, const struct sockaddr_in client) {
+    int len = sizeof(client);
+    sendto(sockfd, hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *) &client, len);
+    exit(EXIT_SUCCESS); // CHILD EXIT
+}
+
+int main(int argc, char ** argv) {
+    int sockfd;
+    char buffer[MAXLINE];
+    struct sockaddr_in servaddr, cliaddr;
+    
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        fprintf(stdout, "line %d: Error caught\n", __LINE__);
+        abort();
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(PORT);
+
+    if (bind(sockfd, (const struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+        fprintf(stdout, "line %d: Error caught\n", __LINE__);
+        abort();
+    }
+
+    int len, n;
+    int pid;
+    len = sizeof(cliaddr);
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        n = recvfrom(sockfd, buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
+        buffer[sizeof(buffer) - 1] = '\0';
+        fprintf(stdout, "Server received message from client: %s\n", buffer);
+        pid = fork();
+        if (pid == 0) {
+            // Let the child handle this client
+            handle_client(sockfd, cliaddr);
+        }
+        fprintf(stdout, "THIS IS THE PARENT PROCESS!\n");
+    }
+    
+    close(sockfd);
+    return(EXIT_SUCCESS);
+}
