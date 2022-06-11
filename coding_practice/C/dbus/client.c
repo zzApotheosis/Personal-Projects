49f308f824fb3b380aa8461d6236144f77a7d8ac
zzApotheosis <zzApotheosis@gmail.com>
2022-06-11 08:50:12 -0500

    Update D-Bus (gdbus) example code
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

--- coding_practice/C/dbus/client.c
+++ coding_practice/C/dbus/client.c
@@ -6,14 +6,14 @@
 #include "testbus.h"
 
 int main(int argc, char ** argv) {
-    TestBusComTestName * proxy;
+    ComTestbus * proxy;
     GError * error;
     gchar * buf[32];
 
     error = NULL;
-    proxy = test_bus_com_test_name_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION, G_DBUS_PROXY_FLAGS_NONE, "com.test.name", "/com/test/name", NULL, &error);
+    proxy = com_testbus_proxy_new_for_bus_sync(G_BUS_TYPE_SESSION, G_DBUS_PROXY_FLAGS_NONE, "com.testbus", "/com/testbus", NULL, &error);
     
-    test_bus_com_test_name_call_hello_world_sync(proxy, "SUPER LIGMA", buf, NULL, &error);
+    com_testbus_call_hello_world_sync(proxy, "SUPER LIGMA", buf, NULL, &error);
     g_print("Received: %s\n", buf[0]);
 
     g_object_unref(proxy);
--- coding_practice/C/dbus/server.c
+++ coding_practice/C/dbus/server.c
@@ -3,23 +3,23 @@
 #include <glib.h>
 #include "testbus.h"
 
-static gboolean on_handle_hello_world (TestBusComTestName * interface, GDBusMethodInvocation * invocation, const gchar * greeting, gpointer user_data) {
+static gboolean on_handle_hello_world (ComTestbus * interface, GDBusMethodInvocation * invocation, const gchar * greeting, gpointer user_data) {
     gchar * response;
     response = g_strdup_printf("Hello world %s!!.", greeting);
-    test_bus_com_test_name_complete_hello_world(interface, invocation, response);
+    com_testbus_complete_hello_world(interface, invocation, response);
     //g_print("%s\n", response);
     g_free(response);
     return(TRUE);
 }
 
 static void on_name_acquired(GDBusConnection * connection, const gchar * name, gpointer user_data) {
-    TestBusComTestName * interface;
+    ComTestbus * interface;
     GError * error;
 
-    interface = test_bus_com_test_name_skeleton_new();
+    interface = com_testbus_skeleton_new();
     g_signal_connect(interface, "handle-hello-world", G_CALLBACK(on_handle_hello_world), NULL);
     error = NULL;
-    !g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON (interface), connection, "/com/test/name", &error);
+    !g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON (interface), connection, "/com/testbus", &error);
 }
 
 int main(int argc, char ** argv) {
@@ -27,7 +27,7 @@ int main(int argc, char ** argv) {
 
     loop = g_main_loop_new(NULL, FALSE);
 
-    g_bus_own_name(G_BUS_TYPE_SESSION, "com.test.name", G_BUS_NAME_OWNER_FLAGS_NONE, NULL,
+    g_bus_own_name(G_BUS_TYPE_SESSION, "com.testbus", G_BUS_NAME_OWNER_FLAGS_NONE, NULL,
                 on_name_acquired, NULL, NULL, NULL);
 
     g_main_loop_run (loop);
