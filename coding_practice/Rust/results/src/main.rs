756271a547088a345b06567aa35ea33e73152622
zzApotheosis <zzApotheosis@gmail.com>
2022-06-25 11:45:51 -0500

    Add Rust results example code
    Signed-off-by: zzApotheosis <zzApotheosis@gmail.com>

---
+++ coding_practice/Rust/results/src/main.rs
@@ -0,0 +1,10 @@
+fn main() -> Result<(), i32> {
+    let x: Result<i32, &str> = Ok(-3);
+    assert_eq!(x.is_ok(), true);
+
+    let x: Result<i32, &str> = Err("Some error message");
+    assert_eq!(x.is_ok(), false);
+
+    // The () is an emptle tuple
+    Ok(())
+}
