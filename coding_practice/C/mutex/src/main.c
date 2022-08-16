3793b5f22d291152b80b9247c9c7d660b655981e
zzApotheosis <zzApotheosis@gmail.com>
2022-08-16 14:33:29 -0500

    Add mutex example in C

---
+++ coding_practice/C/mutex/src/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define ITER 100000
+#define NUM_THREADS 4
+
+int shared = 0;
+pthread_mutex_t shared_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void incr_func(int id) {
+    pthread_mutex_lock(&shared_mutex);
+    shared++;
+    pthread_mutex_unlock(&shared_mutex);
+}
+
+void * func(void * arg) {
+    int id = *(int *) arg;
+    fprintf(stdout, "Inrementing shared value...\n");
+    for (int i = 0; i < ITER; i++) {
+        incr_func(id);
+    }
+    return(NULL);
+}
+
+int main(int argc, char * argv[]) {
+    pthread_t threads[NUM_THREADS];
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, func, &i);
+    }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    fprintf(stdout, "shared = %d\n", shared);
+    return(EXIT_SUCCESS);
+}
