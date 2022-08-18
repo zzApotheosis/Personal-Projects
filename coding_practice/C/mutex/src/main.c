9b27fc94f05b731b45d9f3206e96391577a214f6
zzApotheosis <zzApotheosis@gmail.com>
2022-08-18 18:16:49 -0500

    Updated mutex example code in C

--- coding_practice/C/mutex/src/main.c
+++ coding_practice/C/mutex/src/main.c
@@ -6,6 +6,17 @@
 #define ITER 100000
 #define NUM_THREADS 4
 
+/*
+ * If you run this sample code, you may find that your processor will report a rather large
+ * number when the first thread finishes executing. This is because all NUM_THREADS threads
+ * are racing each other to increment the shared data ITER times. Once the first thread finishes
+ * its incrementing work, the other threads will have also incremented a large portion of their
+ * share of iterations, which adds up to a rather large initial reported value. The important
+ * thing to understand is that every thread respects each others' access to the shared data
+ * through the mutex. Because of this, the program is expected to end with a data value equal to
+ * ITER * NUM_THREADS.
+ */
+
 int shared = 0;
 pthread_mutex_t shared_mutex = PTHREAD_MUTEX_INITIALIZER;
 
@@ -15,11 +26,18 @@ void incr_func() {
     pthread_mutex_unlock(&shared_mutex); // TRY REMOVING ME
 }
 
+void print_func() {
+    pthread_mutex_lock(&shared_mutex); // TRY REMOVING ME
+    fprintf(stdout, "The shared value is %d at the end of thread ID #%lu\n", shared, pthread_self());
+    pthread_mutex_unlock(&shared_mutex); // TRY REMOVING ME
+}
+
 void * func(void * arg) {
     fprintf(stdout, "Inrementing shared value...\n");
     for (int i = 0; i < ITER; i++) {
         incr_func();
     }
+    print_func();
     return(NULL);
 }
 
