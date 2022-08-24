60fb862fe319bb04ad3e22c79f8c4a59084a9129
zzApotheosis <zzApotheosis@gmail.com>
2022-08-24 09:10:08 -0500

    Update function pointer example code

--- coding_practice/C/function_pointers/main.c
+++ coding_practice/C/function_pointers/main.c
@@ -4,23 +4,34 @@
 struct point {
     int x;
     int y;
-    void (*print)(const struct point*);
 };
 
-void print_x(const struct point* p) {
-    fprintf(stdout, "x=%d\n", p->x);
+void print_point(const struct point * p, const unsigned char name[]) {
+    fprintf(stdout, "Point %s: {%d, %d}\n", name, p->x, p->y);
 }
 
-void print_y(const struct point* p) {
-    fprintf(stdout, "y=%d\n", p->y);
+void print_x(const struct point * p, const unsigned char name[]) {
+    fprintf(stdout, "Point %s: x=%d\n", name, p->x);
 }
 
-int main(int argc, char** argv) {
-    struct point p1 = {2, 4, print_x};
-    struct point p2 = {7, 1, print_y};
+void print_y(const struct point * p, const unsigned char name[]) {
+    fprintf(stdout, "Point %s: y=%d\n", name, p->y);
+}
 
-    p1.print(&p1);
-    p2.print(&p2);
+int main(int argc, char * argv[]) {
+    struct point p1 = {2, 4};
+    struct point p2 = {7, 1};
+    void (* func_ptr)(const struct point *, const unsigned char *);
+
+    func_ptr = print_point;
+    func_ptr(&p1, "p1");
+    func_ptr(&p2, "p2");
+    func_ptr = print_x;
+    func_ptr(&p1, "p1");
+    func_ptr(&p2, "p2");
+    func_ptr = print_y;
+    func_ptr(&p1, "p1");
+    func_ptr(&p2, "p2");
 
     return EXIT_SUCCESS;
 }
