62688ef299cd8136c53f11e0e3acd46822435158
zzApotheosis <zzApotheosis@gmail.com>
2023-02-06 19:44:31 -0600

    Fix text spacing

--- coding_practice/C/ansi_escapes/main.c
+++ coding_practice/C/ansi_escapes/main.c
@@ -7,7 +7,7 @@ int main(int argc, char * argv[]) {
         for (j = 0; j < 10; j++) {
             n = 10 * i + j;
             if (n > 108) break;
-            fprintf(stdout, "\033[%dm%3d\033[m", n, n);
+            fprintf(stdout, "\033[%dm %3d\033[m", n, n);
         }
         fprintf(stdout, "\n");
     }
