59ed29cec8fd3adff809bb2806ee8e4d0757e9ad
zzApotheosis <zzApotheosis@gmail.com>
2023-02-28 19:25:44 -0600

    Update template C code

--- coding_practice/C/template.c
+++ coding_practice/C/template.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char ** argv) {
+int main(int argc, char * argv[]) {
+    fprintf(stdout, "Hello, world!\n");
     return(EXIT_SUCCESS);
 }
