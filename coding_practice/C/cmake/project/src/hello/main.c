17f5baae7728f23f5ea028d2c22dbdae2ba94a61
Steven Jennings <zzApotheosis@gmail.com>
2023-10-23 18:08:31 -0500

    Updated CMake example again

---
+++ coding_practice/C/cmake/project/src/hello/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mylib.h" // Header file exists somewhere else
+
+int main(int argc, char * argv[]) {
+  // Define method variables
+  int exit_code = 0;
+
+  hello();
+
+  // Done
+  return exit_code;
+}
---
+++ coding_practice/C/cmake/project/src/mylib/mylib.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "mylib.h"
+
+void hello() {
+  fprintf(stdout, "Hello, world!\n");
+  fprintf(stdout, "Ligma balls\n");
+}
