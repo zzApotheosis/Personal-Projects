5855dee2d9a08702badef137210bf30b6da27833
Steven Jennings <zzApotheosis@gmail.com>
2023-11-19 20:45:07 -0600

    Updated Qt6 + libgcrypt encryption example

--- coding_practice/C++/basic_encryption_gui/src/main.cpp
+++ coding_practice/C++/basic_encryption_gui/src/main.cpp
@@ -1,6 +1,3 @@
-#include <vector>
-#include <iostream>
-
 #include "cipherhandler.hpp"
 #include "mainwindow.hpp"
 
@@ -8,19 +5,6 @@
 
 int main(int argc, char *argv[]) {
         QApplication app(argc, argv);
-
-        CipherHandler::initialize();
-        CipherHandler cipherhandler;
-        cipherhandler.set_passphrase("test passphrase");
-        
-        std::vector<unsigned char> plaintext = CipherHandler::from_string("LIGMA BALLS");
-        std::vector<unsigned char> ciphertext = cipherhandler.encrypt(plaintext);
-
-        //cipherhandler.set_passphrase("other passphrase");
-
-        std::vector<unsigned char> decrypted = cipherhandler.decrypt(ciphertext);
-        std::string decrypted_string = CipherHandler::to_string(decrypted);
-        std::cout << decrypted_string << std::endl;
 
         MainWindow w;
         w.show();
--- coding_practice/C++/basic_encryption_gui/src/mainwindow.cpp
+++ coding_practice/C++/basic_encryption_gui/src/mainwindow.cpp
@@ -13,7 +13,6 @@ MainWindow::MainWindow(QWidget * parent) :
         ui(new Ui::MainWindow)
 {
         ui->setupUi(this);
-        CipherHandler::initialize();
 }
 
 MainWindow::~MainWindow() { delete ui; }
