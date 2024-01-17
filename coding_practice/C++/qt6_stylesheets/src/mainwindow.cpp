609149f8ac7bd88c599ee303ad1344fbdb58f849
Steven Jennings <zzApotheosis@gmail.com>
2024-01-17 13:51:08 -0600

    Updated Qt style sheet example

--- coding_practice/C++/qt6_stylesheets/src/mainwindow.cpp
+++ coding_practice/C++/qt6_stylesheets/src/mainwindow.cpp
@@ -21,37 +21,47 @@ MainWindow::~MainWindow() {
  *       signals with QObject::connect().
  */
 void MainWindow::on_pushButton_blue_clicked() {
-        qApp->setStyleSheet(
-                        "QPushButton { "
-                        "       background-color: white;"
-                        "       border-style: outset;"
-                        "       border-width: 2px;"
-                        "       border-radius: 10px;"
-                        "       border-color: blue;"
-                        "       font: bold 14px;"
-                        "       min-width: 10em;"
-                        "       padding: 6px;"
-                        "}"
-                        ""
-                        "QLabel {"
-                        "       color: blue;"
-                        "}");
+        QString s =
+                "QPushButton { "
+                "       background-color: white;"
+                "       border-style: outset;"
+                "       border-width: 2px;"
+                "       border-radius: 10px;"
+                "       border-color: blue;"
+                "       font: bold 14px;"
+                "       min-width: 10em;"
+                "       padding: 6px;"
+                "}"
+                ""
+                "QPushButton:pressed {"
+                "       background-color: #c0c0ff;"
+                "}"
+                ""
+                "QLabel {"
+                "       color: blue;"
+                "}";
+        qApp->setStyleSheet(s);
 }
 
 void MainWindow::on_pushButton_red_clicked() {
-        qApp->setStyleSheet(
-                        "QPushButton { "
-                        "       background-color: white;"
-                        "       border-style: outset;"
-                        "       border-width: 2px;"
-                        "       border-radius: 10px;"
-                        "       border-color: red;"
-                        "       font: bold 14px;"
-                        "       min-width: 10em;"
-                        "       padding: 6px;"
-                        "}"
-                        ""
-                        "QLabel {"
-                        "       color: red;"
-                        "}");
+        QString s = 
+                "QPushButton { "
+                "       background-color: white;"
+                "       border-style: outset;"
+                "       border-width: 2px;"
+                "       border-radius: 10px;"
+                "       border-color: red;"
+                "       font: bold 14px;"
+                "       min-width: 10em;"
+                "       padding: 6px;"
+                "}"
+                ""
+                "QPushButton:pressed {"
+                "       background-color: #ffc0c0;"
+                "}"
+                ""
+                "QLabel {"
+                "       color: red;"
+                "}";
+        qApp->setStyleSheet(s);
 }
