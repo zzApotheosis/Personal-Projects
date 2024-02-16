668e6aaff3eefdd6fabdfaab1f2ccdb40d49b0de
Steven Jennings <zzApotheosis@gmail.com>
2024-02-16 11:17:14 -0600

    Updated pthreads example with pthread_cond_t

--- coding_practice/C/pthreads/src/main.c
+++ coding_practice/C/pthreads/src/main.c
@@ -3,13 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
+
+#define warn(f, m) \
+        do {\
+                fprintf(f, "%s:%d %s\n", __FILE__, __LINE__, m);\
+        } while (0);
+#define die(f, m) \
+        do {\
+                warn(f, m);\
+                abort();\
+        } while (0);
 
 #ifndef NUM_THREADS
-#define NUM_THREADS 4
+#define NUM_THREADS 64
 #endif
 
 #ifndef MAX_VALUE
-#define MAX_VALUE 1000000
+#define MAX_VALUE 1337
+#endif
+
+#ifndef MAX_SLEEP_TIME
+#define MAX_SLEEP_TIME 500000
 #endif
 
 static int thread_count = 0;
@@ -17,37 +32,53 @@ static pthread_mutex_t thread_count_mutex = PTHREAD_MUTEX_INITIALIZER;
 static int data = 0;
 static pthread_mutex_t data_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *thread_main(void *arg) {
-  pthread_mutex_lock(&thread_count_mutex);
-  int thread_id = thread_count++;
-  pthread_mutex_unlock(&thread_count_mutex);
-  fprintf(stdout, "Initializing thread #%d\n", thread_id);
+static pthread_cond_t data_incremented = PTHREAD_COND_INITIALIZER;
 
-  /* We are now at the mercy of the kernel scheduler */
-  for (;;) {
-    pthread_mutex_lock(&data_mutex);
-    if (data >= MAX_VALUE) {
-        pthread_mutex_unlock(&data_mutex);
-        break;
-    }
-    data++;
-    fprintf(stdout, "Thread #%d incremented the shared data to %d\n", thread_id,
-            data);
-    pthread_mutex_unlock(&data_mutex);
-  }
+void * thread_main(void * arg) {
+        if (arg) {} /* Completely unnecessary arg check but it makes GCC and Clang shut up about unused arguments */
+        pthread_mutex_lock(&thread_count_mutex);
+        int thread_id = thread_count++;
+        pthread_mutex_unlock(&thread_count_mutex);
+        fprintf(stdout, "Initializing thread #%d\n", thread_id);
 
-  return NULL;
+        /* We are now at the mercy of the kernel scheduler */
+        for (;;) {
+                usleep(rand() % MAX_SLEEP_TIME);
+                pthread_mutex_lock(&data_mutex);
+                if (data >= MAX_VALUE) {
+                        pthread_mutex_unlock(&data_mutex);
+                        break;
+                }
+                data++;
+                fprintf(stdout, "Thread #%02d incremented the shared data to %d\n",
+                                thread_id, data);
+                pthread_mutex_unlock(&data_mutex);
+                pthread_cond_broadcast(&data_incremented);
+        }
+
+        return NULL;
 }
 
-int main(int argc, char *argv[]) {
-  fprintf(stdout, "Starting main thread\n");
-  pthread_t threads[NUM_THREADS];
-  for (int i = 0; i < NUM_THREADS; i++) {
-    pthread_create(&threads[i], 0, thread_main, 0);
-  }
-  for (int i = 0; i < NUM_THREADS; i++) {
-    pthread_join(threads[i], NULL);
-  }
-  fprintf(stdout, "Main thread finished\n");
-  return EXIT_SUCCESS;
+int main(void) {
+        srand((unsigned int) time(NULL));
+        
+        fprintf(stdout, "Starting main thread\n");
+        pthread_t threads[NUM_THREADS];
+        for (int i = 0; i < NUM_THREADS; i++) {
+                pthread_create(&threads[i], 0, thread_main, &i);
+        }
+
+        /* Wait for threads to finish */
+        pthread_mutex_lock(&data_mutex);
+        for (;data < MAX_VALUE;) {
+                pthread_cond_wait(&data_incremented, &data_mutex);
+        }
+        pthread_mutex_unlock(&data_mutex);
+        
+        for (int i = 0; i < NUM_THREADS; i++) {
+                fprintf(stdout, "Joining thread #%d\n", i);
+                pthread_join(threads[i], NULL);
+        }
+        fprintf(stdout, "Main thread finished\n");
+        return EXIT_SUCCESS;
 }
