99b9bacb45244d27d4ed19d9208910de9f545322
Steven Jennings <zzApotheosis@gmail.com>
2024-03-21 00:06:57 -0500

    Updated Emacs config
    This is actually starting to amaze me. Emacs is insanely powerful

--- devconfig/.emacs.d/init.el
+++ devconfig/.emacs.d/init.el
@@ -6,20 +6,27 @@
 ;; Disable the startup screen you fucking donkey
 (setq-default inhibit-startup-screen t)
 
-;; Never use tabs you fucking donkey
+;; Never use tabs, the menu bar, the tool bar, or the scroll bar you fucking donkey
 (indent-tabs-mode 0)
-
-;; Don't use the menu bar you fucking donkey
 (menu-bar-mode 0)
-
-;; Don't use the tool bar you fucking donkey
 (tool-bar-mode 0)
-
-;; Don't use the scroll bar you fucking donkey
 (scroll-bar-mode 0)
 
 ;; Define eshell history size
 (setq-default eshell-history-size 25000)
+
+;; Define more load-path items
+(add-to-list 'load-path "~/.emacs.d/pkgs/auto-complete")
+
+;; Load manually installed packages
+(use-package auto-complete)
+
+;; Default to Emacs keybindings but have Evil mode installed
+(evil-mode t)
+(setq-default evil-default-state 'emacs)
+
+;; Enable Vertico mode
+(vertico-mode t)
 
 ;; Define tab-stop-list
 (let ((limit 120)
@@ -34,6 +41,8 @@
 (add-hook 'c-mode-common-hook
           #'(lambda ()
               ;; Put stuff here
+	      (eglot-ensure)
+	      (auto-complete-mode t)
               ))
 
 ;; Define hook for prog-mode
@@ -94,7 +103,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Man-notify-method 'pushy)) ;; I want man pages to follow in the current buffer
+ '(Man-notify-method 'pushy)
+ '(package-selected-packages
+   '(org rust-mode flycheck cl-lib goto-chg eglot dash multishell)))
+ ;; I want man pages to follow in the current buffer
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
