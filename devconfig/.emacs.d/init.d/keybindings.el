982acf95b4851c170ceea8ae1289231e074357f6
Steven Jennings <zzApotheosis@gmail.com>
2024-04-06 17:49:43 -0500

    Update Emacs config

--- devconfig/.emacs.d/init.d/keybindings.el
+++ devconfig/.emacs.d/init.d/keybindings.el
@@ -1,2 +1,7 @@
 ;; Create personal keybindings
+
+;; A convenience keybinding for switching between emacs mode and evil mode when evil-mode is enabled
 (keymap-global-set "ESC ESC z" 'evil-local-mode)
+
+;; A keybinding to quit Emacs, but also kill the daemon with the client
+(keymap-global-set "C-x c ESC" 'save-buffers-kill-emacs)
