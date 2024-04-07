b369a1a3f2ff73f73e4cf48f715010396a073220
Steven Jennings <zzApotheosis@gmail.com>
2024-04-06 23:09:06 -0500

    Update Emacs config
    Damn this is addicting

--- devconfig/.emacs.d/init.d/keybindings.el
+++ devconfig/.emacs.d/init.d/keybindings.el
@@ -6,5 +6,7 @@
 ;; A keybinding to quit Emacs, but also kill the daemon with the client
 (keymap-global-set "C-x c k" 'save-buffers-kill-emacs)
 
+;; All "theme" related keybindings
 ;; A keybinding to disable all themes (useful on the terminal when I want to let my terminal emulator handle colors)
 (keymap-global-set "C-x c t d" 'disable-all-themes)
+(keymap-global-set "C-x c t c" 'customize-themes)
--- devconfig/.emacs.d/init.d/pkg-setup.d/setup-erc.el
+++ devconfig/.emacs.d/init.d/pkg-setup.d/setup-erc.el
@@ -15,3 +15,16 @@
 ;;		   (set-buffer buffer)
 ;;		   (when (eq major-mode 'erc-mode)
 ;;		     (setq erc-fill-column (- (window-width w) 2)))))))))
+
+;; We want to kill buffers on /part
+(setq-default erc-kill-buffer-on-part t)
+
+;; Load custom IRC settings in private file
+;; NOTE: This probably is a bad approach to this problem. I need to learn how auth-sources works and do that instead. So I'll leave this commented out for now
+;;(let (
+;;      (erc-creds-file (concat init-dir "/erc-private.el"))
+;;      )
+;;  (when (file-exists-p erc-creds-file)
+;;    (load-file erc-creds-file)
+;;    )
+;;  )
