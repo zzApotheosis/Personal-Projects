be559e393a1008fd9bd33dfaba3b6d1799f55885
Steven Jennings <zzApotheosis@gmail.com>
2024-04-14 11:23:42 -0500

    Updated Emacs config

--- devconfig/.emacs.d/init.d/keybindings.el
+++ devconfig/.emacs.d/init.d/keybindings.el
@@ -4,7 +4,7 @@
 ;; A convenience keybinding for switching between emacs mode and evil mode when evil-mode is enabled
 (keymap-global-set "C-x c e l" 'evil-local-mode)
 ;; A convenience keybinding for evil-mode
-(keymap-global-set "C-x c e e" 'evil-mode)
+(keymap-global-set "C-x c e t" 'evil-mode)
 
 ;; A keybinding to quit Emacs, but also kill the daemon with the client
 (keymap-global-set "C-x c k" 'save-buffers-kill-emacs)
@@ -13,3 +13,10 @@
 ;; A keybinding to disable all themes (useful on the terminal when I want to let my terminal emulator handle colors)
 (keymap-global-set "C-x c t d" 'disable-all-themes)
 (keymap-global-set "C-x c t c" 'customize-themes)
+
+;; All Neotree related keybindings
+(keymap-global-set "C-x c <TAB> s" 'neotree-show)
+(keymap-global-set "C-x c <TAB> h" (lambda ()
+				     (interactive)
+				     (neotree-show)
+				     (neotree-hide)))
