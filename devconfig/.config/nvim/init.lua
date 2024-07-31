f5a60db6485197794c3f9ef325c3c98c8d205f0c
Steven Jennings <zzApotheosis@gmail.com>
2024-07-31 15:35:38 -0500

    Updated Neovim config

--- devconfig/.config/nvim/init.lua
+++ devconfig/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 -- Set standard Neovim configuration options
 vim.opt.number = true
 -- vim.opt.relativenumber = true
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.ai = true
 vim.opt.si = true
@@ -13,73 +13,82 @@ vim.opt.foldmethod = 'indent'
 -- Utilize lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Define desired plugins
 local plugins = {
-    -- oil.nvim
-    {
-        'stevearc/oil.nvim',
-        opts = {},
-        -- Optional dependencies
-        dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function ()
-                require("oil").setup()
-        end
-    }
+  -- oil.nvim
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    config = function ()
+      require("oil").setup()
+    end
+  }
 
-    -- leap.nvim
-    ,{
-        'ggandor/leap.nvim',
-        opts = {},
-        config = function()
-                require('leap').create_default_mappings()
-        end
-    }
+  -- leap.nvim
+  ,{
+    'ggandor/leap.nvim',
+    opts = {},
+    config = function()
+      require('leap').create_default_mappings()
+    end
+  }
 
-    -- project.nvim
-    ,{
-       'ahmedkhalf/project.nvim',
-        opts = {},
-        config = function()
-    	require("project_nvim").setup()
-    	end
-    }
+  -- project.nvim
+  ,{
+   'ahmedkhalf/project.nvim',
+    opts = {},
+    config = function()
+  	  require("project_nvim").setup()
+  	end
+  }
 
-    -- nvim-tree.lua
-    ,{
-        'nvim-tree/nvim-tree.lua',
-        opts = {},
-        dependencies = {"nvim-tree/nvim-web-devicons"},
-        config = function()
-            require("nvim-tree").setup()
-        end
-    }
+  -- nvim-tree.lua
+  ,{
+    'nvim-tree/nvim-tree.lua',
+    opts = {},
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    config = function()
+      require("nvim-tree").setup()
+    end
+  }
 
-    -- ale
-    ,{
-        'dense-analysis/ale',
-        opts = {},
-        config = function()
-            -- Do nothing
-        end
-    }
+  -- ale
+  ,{
+    'dense-analysis/ale',
+    opts = {},
+    config = function()
+      -- Do nothing
+    end
+  }
+
+  -- vimwiki
+  ,{
+    '',
+    opts = {},
+    config = function()
+      -- Do nothing
+    end
+  }
 }
 
 -- Invoke lazy.nvim to setup desired plugins
 require("lazy").setup(plugins, {})
 
 -- vim.lsp.start({
---     name = 'my-server-name',
---     cmd = {'clangd'},
---     root_dir = "/home/zzapotheosis/Development/Git/Personal-Projects/coding_practice/C"
+--   name = 'my-server-name',
+--   cmd = {'clangd'},
+--   root_dir = "/home/zzapotheosis/Development/Git/Personal-Projects/coding_practice/C"
 -- })
