ee2ea99101c35555b71f233f17b1ac69742117e3
Steven Jennings <zzApotheosis@gmail.com>
2024-08-18 08:34:11 -0500

    Updated libgmp example

--- coding_practice/C/libgmp/src/main.c
+++ coding_practice/C/libgmp/src/main.c
@@ -20,7 +20,7 @@ int main(int argc, char * argv[]) {
   
   /* Assignment */
   mpz_set_ui(x, 0x1337);
-  mpz_set_si(y, -69420);
+  mpz_set_si(y, 69420);
   mpz_set_str(z,
       "214037531996711403489735576201333780809818562656253"
       "361013084186849597122035276377524870741016199384551"
@@ -32,17 +32,39 @@ int main(int argc, char * argv[]) {
       "967704363463795702362816279868725722795383379120504"
       "375833195321502341190822242620725216073633790270933"
       "435241526519392700118934494343612062001192396318634", 0);
+
   
   /* Use output functions */
   gmp_printf("%s is an mpz: %Zx\n", "x", x);
   gmp_printf("%s is an mpz: %Zd\n", "y", y);
   gmp_printf("%s is an mpz: %Zd\n", "z", z);
+  fprintf(stdout, "\n");
+
+  /* Show the power of this epic math library */
+  mpz_pow_ui(z, x, 42);
+  gmp_printf("%s is now: %Zd\n", "z", z);
+  fprintf(stdout, "\n");
+
+  mpz_t divisor;
+  mpz_init(divisor);
+  mpz_set_ui(divisor, 1000);
+  for (;;) {
+    gmp_printf("%Zd / %Zd = ", z, divisor);
+    mpz_cdiv_q(z, z, divisor);
+    gmp_printf("%Zd\n", z);
+    fprintf(stdout, "\n");
+
+    if (mpz_cmp_ui(z, 1) <= 0) {
+      break;
+    }
+  }
 
   /* Always be cognizant of memory usage */
   //mpz_clear(x);
   //mpz_clear(y);
   //mpz_clear(z);
-  mpz_clears(x, y, z, NULL); /* Equivalent to the three statements above */
+  //mpz_clear(divisor);
+  mpz_clears(x, y, z, divisor, NULL); /* Equivalent to the three statements above */
 
   /* 
    * I recently learned that the C standard dictates that if the main()
