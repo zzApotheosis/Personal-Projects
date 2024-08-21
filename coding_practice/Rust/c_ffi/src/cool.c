e270545cf4e0f2c1e79efc53bf0f42c496b0637d
Steven Jennings <zzApotheosis@gmail.com>
2024-08-20 19:34:09 -0500

    Updated C code for Rust C FFI example
    I learned how to further abstract C source implementation
    from my time at GaTech. It turns out, the header can simply
    declare a struct type and typedef it and the source file
    can define what's actually in the struct so long as the
    function signatures in the header use ONLY a pointer to the
    struct declaration.

--- coding_practice/Rust/c_ffi/src/cool.c
+++ coding_practice/Rust/c_ffi/src/cool.c
@@ -3,6 +3,11 @@
 
 #include "cool.h"
 
+struct CoolStruct {
+    int x;
+    int y;
+};
+
 void cool_function(int i, char c, CoolStruct * cs) {
     fprintf(stdout, "i = %d\n", i);
     fprintf(stdout, "c = %c\n", c);
--- coding_practice/Rust/c_ffi/src/cool.h
+++ coding_practice/Rust/c_ffi/src/cool.h
@@ -1,10 +1,7 @@
 #ifndef COOL_H
 #define COOL_H
 
-typedef struct CoolStruct {
-    int x;
-    int y;
-} CoolStruct;
+typedef struct CoolStruct CoolStruct;
 
 void cool_function(int i, char c, CoolStruct * cs);
 
