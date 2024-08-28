0323d6d10899719647c24d9287c182915c6aa562
Steven Jennings <zzApotheosis@gmail.com>
2024-08-28 10:26:44 -0500

    Updated C FFI example in Rust

--- coding_practice/Rust/c-ffi/src/cool.c
+++ coding_practice/Rust/c-ffi/src/cool.c
@@ -3,6 +3,8 @@
 
 #include "cool.h"
 
+static int x = 0;
+
 struct CoolStruct {
     int x;
     int y;
@@ -11,8 +13,13 @@ struct CoolStruct {
 void cool_function(int i, char c, CoolStruct * cs) {
     fprintf(stdout, "i = %d\n", i);
     fprintf(stdout, "c = %c\n", c);
+    x = i;
     if (cs != NULL) {
         fprintf(stdout, "x = %d\n", cs->x);
         fprintf(stdout, "y = %d\n", cs->y);
     }
+}
+
+int get_x(void) {
+    return x;
 }
--- coding_practice/Rust/c-ffi/src/main.rs
+++ coding_practice/Rust/c-ffi/src/main.rs
@@ -6,16 +6,24 @@ pub struct CoolStruct {
 
 extern "C" {
     pub fn cool_function(i: cty::c_int, c: cty::c_char, cs: *mut CoolStruct);
+    pub fn get_x() -> i32;
 }
 
 fn c_cool_function(i: cty::c_int, c: cty::c_char, cs: *mut CoolStruct) {
     unsafe {
         cool_function(i, c, cs);
-    };
+    }
 }
 
 fn main() -> Result<(), i32> {
     let mut s = CoolStruct { x: 69, y: 11 };
     c_cool_function(1, 0x23, &mut s);
-    Ok(())
+    c_cool_function(10, 0x23, &mut s);
+    let mut x = 100;
+    println!("x = {}", x);
+    unsafe {
+        x = get_x();
+    }
+    println!("x = {}", x);
+    return Ok(());
 }
