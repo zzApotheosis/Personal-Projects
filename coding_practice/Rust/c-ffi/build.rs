00a897fb19795850326c6b26a6953c18e5b3fffe
Steven Jennings <zzApotheosis@gmail.com>
2024-08-28 10:31:25 -0500

    Updated Rust C FFI build script
    This neat trick tells Cargo to recompile specific C libraries if it
    finds that they have changed.

--- coding_practice/Rust/c-ffi/build.rs
+++ coding_practice/Rust/c-ffi/build.rs
@@ -1,5 +1,8 @@
 extern crate cc;
 
 fn main() {
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    println!("cargo:rerun-if-changed=src/cool.c");
+    println!("cargo:rerun-if-changed=src/cool.h");
     cc::Build::new().file("src/cool.c").compile("libcool.a");
 }
