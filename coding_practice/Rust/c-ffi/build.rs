41c94b233fd7abf0b2a1c7a08300a346ffa0d61c
Steven Jennings <zzApotheosis@gmail.com>
2024-08-28 21:09:19 -0500

    Updated build script for Rust C FFI example

--- coding_practice/Rust/c-ffi/build.rs
+++ coding_practice/Rust/c-ffi/build.rs
@@ -1,6 +1,9 @@
-fn main() {
+fn main() -> Result<(), i32> {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=src/cool.c");
     println!("cargo:rerun-if-changed=src/cool.h");
-    cc::Build::new().file("src/cool.c").compile("libcool.a");
+    let mut c_compiler = cc::Build::new();
+    c_compiler.file("src/cool.c");
+    c_compiler.compile("libcool.a");
+    Ok(())
 }
