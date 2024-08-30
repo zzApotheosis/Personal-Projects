83cceb772a7b736bc43906578a3d6fd1060b7d74
Steven Jennings <zzApotheosis@gmail.com>
2024-08-30 11:37:32 -0500

    Updated Rust Unix socket example
    Learning more about ownership and borrowing. This
    is actually really cool.

--- coding_practice/Rust/unix-sockets/src/client/main.rs
+++ coding_practice/Rust/unix-sockets/src/client/main.rs
@@ -1,11 +1,39 @@
+use core::panic;
 use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
+use std::time::Duration;
+use std::vec::Vec;
+
+const SOCKET: &str = "/tmp/rustsock";
+const DELAY: Duration = Duration::new(0u64, 0_250_000_000u32);
 
 fn main() -> std::io::Result<()> {
-    let mut stream = UnixStream::connect("/tmp/rustsock")?;
-    // stream.write_all(b"hello world")?;
-    let mut response = String::new();
-    stream.read_to_string(&mut response)?;
-    println!("{response}");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Pass some arguments to this program, freaking noob.");
+    }
+
+    let mut stream: UnixStream = UnixStream::connect(SOCKET)?;
+    let mut msg: String = std::string::String::new();
+
+    for i in 1..args.len() {
+        stream.write_all(args[i].as_bytes())?;
+        stream.write(b"\n")?;
+
+        let mut reader = std::io::BufReader::new(&mut stream);
+        msg.clear();
+        reader.read_line(&mut msg)?;
+        let msg = msg.trim();
+        println!("Received response: {msg}");
+
+        std::thread::sleep(DELAY);
+    }
+
+    /*
+     * In this implementation, I am defining that the server should close the
+     * connection when the server receives an empty message, delimited by
+     * the newline character "\n".
+     */
+    stream.write_all(b"\n")?;
     Ok(())
 }
--- coding_practice/Rust/unix-sockets/src/server/main.rs
+++ coding_practice/Rust/unix-sockets/src/server/main.rs
@@ -1,27 +1,53 @@
-use std::io::Write;
+use std::io::{BufRead, Write};
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::thread;
 
-fn handle_client(mut stream: UnixStream) {
-    // ...
-    // stream
-    stream.write_all(b"what").expect("NOT GOOD");
-    stream.shutdown(std::net::Shutdown::Both).expect("NOT GOOD");
+const SOCKET: &str = "/tmp/rustsock";
+
+fn handle_client(stream: UnixStream) -> std::io::Result<()> {
+    let mut reader = std::io::BufReader::new(&stream); // I don't trust this
+
+    /*
+     * NOTE: This implementation uses the newline "\n" character
+     * as a delimiter in the communication. Otherwise, the stream
+     * would wait forever until it received EOF to act on the data
+     * received. As our favorite cringe lord once said in a certain
+     * prequel movie, "this is where the fun begins".
+     */
+    loop {
+        let mut msg: String = String::new();
+        reader.read_line(&mut msg)?;
+        let msg = msg.trim();
+        if msg.len() == 0usize {
+            break;
+        }
+        println!("Received message: {msg}");
+        // writeln!(&stream, "{}", msg)?;
+        (&stream).write_all(msg.as_bytes())?;
+        (&stream).write_all(b"\n")?;
+    }
+
+    stream.shutdown(std::net::Shutdown::Both)?;
+    Ok(())
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = UnixListener::bind("/tmp/rustsock")?;
+    let listener = UnixListener::bind(SOCKET)?;
 
     // accept connections and process them, spawning a new thread for each one
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 /* connection succeeded */
-                thread::spawn(|| handle_client(stream));
+                thread::spawn(|| {
+                    handle_client(stream).expect("NOT GOOD");
+                    println!("Client finished");
+                });
             }
+
             Err(err) => {
                 /* connection failed */
-                eprintln!("{}", err);
+                eprintln!("{err}");
                 break;
             }
         }
