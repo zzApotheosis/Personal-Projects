4e32015d3406a86678dbda3189b4455134f8ad07
Steven Jennings <zzApotheosis@gmail.com>
2024-10-02 17:06:37 -0500

    Still working on Zig Unix socket example

--- coding-practice/Zig/unix-sockets/build.zig
+++ coding-practice/Zig/unix-sockets/build.zig
@@ -15,9 +15,15 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "unix-sockets",
-        .root_source_file = b.path("src/main.zig"),
+    const server_exe = b.addExecutable(.{
+        .name = "server",
+        .root_source_file = b.path("src/server.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const client_exe = b.addExecutable(.{
+        .name = "client",
+        .root_source_file = b.path("src/client.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -25,12 +31,13 @@ pub fn build(b: *std.Build) void {
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
-    b.installArtifact(exe);
+    b.installArtifact(server_exe);
+    b.installArtifact(client_exe);
 
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(server_exe);
 
     // By making the run step depend on the install step, it will be run from the
     // installation directory rather than directly from within the cache directory.
@@ -50,13 +57,13 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+    const server_exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/server.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_exe_unit_tests = b.addRunArtifact(server_exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
--- coding-practice/Zig/unix-sockets/src/server.zig
+++ coding-practice/Zig/unix-sockets/src/server.zig
@@ -13,18 +13,26 @@ pub fn main() void {
 
     std.fs.cwd().deleteFile(socket_path) catch |e| {
         switch (e) {
-            error.FileNotFound => {},
+            error.FileNotFound => {}, // If there was no file to begin with, it's fine
             else => {
                 std.debug.print("error: {}\n", .{e});
             },
         }
     };
 
-    const server: ?std.net.Server = std.net.Address.listen(address.?, .{ .reuse_address = true }) catch |e| a: {
+    var server: ?std.net.Server = std.net.Address.listen(address.?, .{ .reuse_address = true }) catch |e| a: {
         std.io.getStdErr().writer().print("error: {}\n", .{e}) catch {};
         break :a null;
     };
     if (server == null) {
         return;
+    }
+
+    while (true) {
+        const client: ?std.net.Server.Connection = server.?.accept() catch |e| a: {
+            std.debug.print("error: {}\n", .{e});
+            break :a null;
+        };
+        defer client.?.stream.close();
     }
 }
