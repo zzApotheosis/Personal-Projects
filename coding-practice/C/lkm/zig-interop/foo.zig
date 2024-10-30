032f64bb737ea220811b60c02472f6d06b9bf27b
Steven Jennings <zzApotheosis@gmail.com>
2024-10-30 16:34:32 -0500

    Added updated Zig/C Linux kernel module example

--- coding-practice/C/lkm/zig-interop/foo.zig
+++ coding-practice/C/lkm/zig-interop/foo.zig
@@ -1,6 +1,16 @@
-var mydata: c_uint = 0;
+const std = @import("std");
 
-pub export fn zig_foo(n: c_uint) callconv(.C) c_uint {
-    mydata = mydata + n;
-    return mydata;
+var rand: std.rand.DefaultPrng = std.rand.DefaultPrng.init(0);
+
+pub export fn zig_increment(n: u32) callconv(.C) u32 {
+    const new_num = n + 1;
+    return new_num;
+}
+
+pub export fn init_rando(seed: c_long) callconv(.C) void {
+    rand = std.rand.DefaultPrng.init(@intCast(seed));
+}
+
+pub export fn gimme_rando() callconv(.C) c_uint {
+    return rand.random().int(c_uint);
 }
