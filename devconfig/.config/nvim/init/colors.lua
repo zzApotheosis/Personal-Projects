71549bd596932e2751858ff54d4c0e306b618a27
Steven Jennings <zzApotheosis@gmail.com>
2024-11-01 18:22:39 -0500

    Enabled transparent background

--- devconfig/.config/nvim/init/colors.lua
+++ devconfig/.config/nvim/init/colors.lua
@@ -5,14 +5,14 @@ local m = {}
 -- Setup function
 function m.setup()
   vim.cmd('colorscheme catppuccin')
-  --m.bg_transparent()
+  m.bg_transparent()
 end
 
 -- Public-facing function to set transparent background
 function m.bg_transparent()
   -- Set transparent background no matter what
-  --vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  --vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
 -- Init function
