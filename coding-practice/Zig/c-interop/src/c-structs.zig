4cfde6baa49ce9b92cab2ca55717537bbe74b14a
Steven Jennings <zzApotheosis@gmail.com>
2024-11-10 13:01:05 -0600

    Updated Zig/C interoperability example
    It turns out, Zig can represent structs internally and still properly
    pass them to C functions with no problems.
    
    God, I hope Zig gets widespread adoption in the future. This language is
    incredibly good.

--- coding-practice/Zig/c-interop/src/c-structs.zig
+++ coding-practice/Zig/c-interop/src/c-structs.zig
@@ -3,6 +3,12 @@ const c_foo = @cImport({
     @cInclude("foo.h");
 });
 
+const ZigPoint = struct {
+    x: c_int = 0,
+    y: c_int = 0,
+    z: c_int = 0,
+};
+
 // Assign types and functions to more convenient names
 const Point = ?*c_foo.Point;
 const Point_new = c_foo.Point_new;
@@ -15,56 +21,59 @@ const Point_get_y = c_foo.Point_get_y;
 const Point_get_z = c_foo.Point_get_z;
 
 pub fn main() void {
-    var points: [2]Point = undefined;
-    for (0..points.len) |i| {
+    const num_points: usize = 4;
+    var points: [num_points]Point = undefined;
+    for (0..num_points) |i| {
         points[i] = Point_new();
         if (points[i] == null) {
             unreachable;
         }
     }
 
+    var zigpoint = ZigPoint{};
+
     const x: c_int = @as(c_int, 1337);
     const y: c_int = @as(c_int, 420);
     const z: c_int = @as(c_int, 69);
 
-    Point_set_x(points[0], x);
-    Point_set_y(points[0], y);
-    Point_set_z(points[0], z);
-    Point_set_x(points[1], x + 1);
-    Point_set_y(points[1], y + 1);
-    Point_set_z(points[1], z + 1);
+    for (0..num_points) |i| {
+        Point_set_x(points[i], x + @as(c_int, @intCast(i)));
+        Point_set_y(points[i], y + @as(c_int, @intCast(i)));
+        Point_set_z(points[i], z + @as(c_int, @intCast(i)));
+    }
+    Point_set_x(@ptrCast(&zigpoint), x);
+    Point_set_y(@ptrCast(&zigpoint), y);
+    Point_set_z(@ptrCast(&zigpoint), z);
 
-    std.debug.assert(Point_get_x(points[0]) == x);
-    std.debug.assert(Point_get_y(points[0]) == y);
-    std.debug.assert(Point_get_z(points[0]) == z);
-    std.debug.assert(Point_get_x(points[1]) == x + 1);
-    std.debug.assert(Point_get_y(points[1]) == y + 1);
-    std.debug.assert(Point_get_z(points[1]) == z + 1);
+    std.debug.print("ZigPoint\n", .{});
+    std.debug.print("x = {}\n", .{Point_get_x(@ptrCast(&zigpoint))});
+    std.debug.print("y = {}\n", .{Point_get_y(@ptrCast(&zigpoint))});
+    std.debug.print("z = {}\n\n", .{Point_get_z(@ptrCast(&zigpoint))});
 
-    std.debug.print("Point 1\n", .{});
-    std.debug.print("x = {}\n", .{Point_get_x(points[0])});
-    std.debug.print("y = {}\n", .{Point_get_y(points[0])});
-    std.debug.print("z = {}\n", .{Point_get_z(points[0])});
-    std.debug.print("\n", .{});
+    for (0..num_points) |i| {
+        std.debug.assert(Point_get_x(points[i]) == x + @as(c_int, @intCast(i)));
+        std.debug.assert(Point_get_y(points[i]) == y + @as(c_int, @intCast(i)));
+        std.debug.assert(Point_get_z(points[i]) == z + @as(c_int, @intCast(i)));
 
-    std.debug.print("Point 2\n", .{});
-    std.debug.print("x = {}\n", .{Point_get_x(points[1])});
-    std.debug.print("y = {}\n", .{Point_get_y(points[1])});
-    std.debug.print("z = {}\n", .{Point_get_z(points[1])});
-    std.debug.print("\n", .{});
+        std.debug.print("Point {}\n", .{i});
+        std.debug.print("x = {}\n", .{Point_get_x(points[i])});
+        std.debug.print("y = {}\n", .{Point_get_y(points[i])});
+        std.debug.print("z = {}\n", .{Point_get_z(points[i])});
+        std.debug.print("\n", .{});
+    }
 
     std.debug.print("Before free()\n", .{});
-    for (0..points.len) |i| {
-        std.debug.print("point {} = {?}\n", .{ i + 1, points[i] });
+    for (0..num_points) |i| {
+        std.debug.print("point {} = {?}\n", .{ i, points[i] });
     }
     std.debug.print("\n", .{});
 
-    for (0..points.len) |i| {
+    for (0..num_points) |i| {
         Point_destroy(&points[i]);
     }
 
     std.debug.print("After free()\n", .{});
-    for (0..points.len) |i| {
-        std.debug.print("point {} = {?}\n", .{ i + 1, points[i] });
+    for (0..num_points) |i| {
+        std.debug.print("point {} = {?}\n", .{ i, points[i] });
     }
 }
