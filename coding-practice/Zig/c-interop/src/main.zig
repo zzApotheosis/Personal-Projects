4a01ea446a30263e761f98503129a0d16331b303
Steven Jennings <zzApotheosis@gmail.com>
2024-11-09 21:50:53 -0600

    Removed unused code in main.zig

--- coding-practice/Zig/c-interop/src/main.zig
+++ coding-practice/Zig/c-interop/src/main.zig
@@ -3,7 +3,7 @@ const c_foo = @cImport({
     @cInclude("foo.h");
 });
 
-pub fn main() !void {
+pub fn main() void {
     call_c_foo();
 }
 
@@ -24,40 +24,4 @@ fn call_c_foo() void {
     const rand = prng.random();
     const num: u32 = rand.intRangeAtMost(u32, 0, 110);
     c_foo.c_foo(num);
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "C struct in Zig" {
-    const Point = ?*c_foo.Point;
-    const Point_new = c_foo.Point_new;
-    const Point_destroy = c_foo.Point_destroy;
-    const Point_set_x = c_foo.Point_set_x;
-    const Point_set_y = c_foo.Point_set_y;
-    const Point_set_z = c_foo.Point_set_z;
-    const Point_get_x = c_foo.Point_get_x;
-    const Point_get_y = c_foo.Point_get_y;
-    const Point_get_z = c_foo.Point_get_z;
-
-    const point: Point = Point_new();
-    if (point == null) {
-        return;
-    }
-
-    const x: c_int = @as(c_int, 1);
-    const y: c_int = @as(c_int, 2);
-    const z: c_int = @as(c_int, 3);
-    _ = Point_set_x(point, x);
-    _ = Point_set_y(point, y);
-    _ = Point_set_z(point, z);
-    std.debug.assert(Point_get_x(point) == x);
-    std.debug.assert(Point_get_y(point) == y);
-    std.debug.assert(Point_get_z(point) == z);
-    std.debug.assert(Point_get_x(point) == 5);
-    Point_destroy(point);
 }
