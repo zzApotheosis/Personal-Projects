d93bc7cbb97f50ee855e2f6a87c828d3454473cd
Steven Jennings <zzApotheosis@gmail.com>
2024-11-10 09:43:03 -0600

    Update example GTK4 code

--- coding-practice/C/gtk4/src/helloworld.c
+++ coding-practice/C/gtk4/src/helloworld.c
@@ -4,18 +4,29 @@
 
 static void print_hello(GtkWidget* widget, gpointer data) {
     g_print("Hello World\n");
+
+    if (data != NULL) {
+        void (*call_data)(void) = data;
+        call_data();
+    } else {
+        fprintf(stderr, "data is null\n");
+    }
+}
+
+static void custom_function(void) {
+    fprintf(stderr, "Reached custom function!\n");
 }
 
 static void activate(GtkApplication* app, gpointer user_data) {
-    GtkWidget* window;
-    GtkWidget* button;
+    GtkWidget * window = NULL;
+    GtkWidget * button = NULL;
     
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
     
     button = gtk_button_new_with_label("Hello World");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), custom_function);
     gtk_window_set_child(GTK_WINDOW(window), button);
 
     gtk_window_present(GTK_WINDOW(window));
@@ -25,9 +36,10 @@ int main(int argc, char** argv) {
     GtkApplication* app;
     int status;
 
-    app = gtk_application_new("local.home.test.helloworld", G_APPLICATION_FLAGS_NONE);
+    app = gtk_application_new("com.ligma", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
+
     g_object_unref(app);
 
     return status;
